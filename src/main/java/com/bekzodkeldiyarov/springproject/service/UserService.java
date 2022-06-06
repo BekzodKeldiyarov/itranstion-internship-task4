e439154165bda11965f74f255d4241c507f55e08
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-06-06 12:23:21 +0500

    ; added

--- src/main/java/com/bekzodkeldiyarov/springproject/service/UserService.java
+++ src/main/java/com/bekzodkeldiyarov/springproject/service/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
 
     public void setLastLoginTime(String username, LocalDateTime time) {
         User user = userRepository.findByUsername(username);
-        user.setLastLoginDate(time);g
+        user.setLastLoginDate(time);
         userRepository.save(user);
     }
 
